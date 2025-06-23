16d7d8b20b214603c39b50229aab275de5bfd892
Priyanka <vandana.official767@gmail.com>
2025-06-23 19:10:36 +0530

    Table of 5 by java 8

--- src/com/java/practice/Counting.java
+++ src/com/java/practice/Counting.java
@@ -1,15 +1,21 @@
 package com.java.practice;
 
-import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Counting {
 	public static void main(String[] args) {
 
-		Scanner scanner= new Scanner(System.in);
-		System.out.println("Enter the table number ");
-		int n= scanner.nextInt();
-		for(int i=1;i<=10;i++) {
-			System.out.println("table of " + n + " = " + n*i);
-		}
-	}
+		//Scanner scanner= new Scanner(System.in);
+		//System.out.println("Enter the table number ");
+		//int n= scanner.nextInt();
+		//for(int i=1;i<=10;i++) {
+			//System.out.println("table of " + n + " = " + n*i);
+		//}
+		//for(int i=1;i<=10;i++) {
+		//System.out.println("table of "+ 5 + "  =  "+ 5 * i);
+	//}
+		 int num = 5;
+	        IntStream.rangeClosed(1, 10)
+	                 .forEach(i -> System.out.println(num + " x " + i + " = " + (num * i)));
+}
 }
