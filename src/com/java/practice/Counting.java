90b6a6e33a73227adf3bfc05b1606afac7c5b91e
Priyanka <vandana.official767@gmail.com>
2025-06-23 20:34:59 +0530

    Table of 5 through java 8 by Scanner

--- src/com/java/practice/Counting.java
+++ src/com/java/practice/Counting.java
@@ -1,5 +1,6 @@
 package com.java.practice;
 
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Counting {
@@ -14,8 +15,11 @@ public class Counting {
 		//for(int i=1;i<=10;i++) {
 		//System.out.println("table of "+ 5 + "  =  "+ 5 * i);
 	//}
-		 int num = 5;
-	        IntStream.rangeClosed(1, 10)
-	                 .forEach(i -> System.out.println(num + " x " + i + " = " + (num * i)));
-}
+		
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Enter the table number ");
+		int n= scanner.nextInt();
+		    IntStream.rangeClosed(1, 10)
+		    .forEach(i -> System.out.println("6 x " + i + " = " + (6 * i)));
+	}
 }
