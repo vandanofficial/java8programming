c79fdfb7a4e1a83793eb37bada1718a8016d43ff
Priyanka <vandana.official767@gmail.com>
2025-06-23 17:57:06 +0530

    added comments

--- src/com/java8/CommonArray.java
+++ src/com/java8/CommonArray.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class CommonArray {
 
 	public static void main(String[] args) {
+		//below programming of stream
 		List<Integer> list1= Arrays.asList(3,4,5,6,7,9,8);
 		List<Integer> list2= Arrays.asList(1,2,3,4,5,22,44);
 		list1.stream().filter(list2::contains)
