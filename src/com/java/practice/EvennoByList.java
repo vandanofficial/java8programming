67c66d9363ae4e7582a4d963b2c224203f23e8b0
Priyanka <vandana.official767@gmail.com>
2025-06-24 21:33:27 +0530

    find even number by list

--- src/com/java/practice/EvennoByList.java
+++ src/com/java/practice/EvennoByList.java
@@ -1,14 +1,70 @@
 package com.java.practice;
 
+import java.awt.dnd.MouseDragGestureRecognizer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//used filter method
 public class EvennoByList {
 	public static void main(String[] args) {
-		List<Integer>data=Arrays.asList(10,5,23,4,6,20,27);
-		List<Integer>newdata=data.stream().filter(n->n%2==0).collect(Collectors.toList());
-		System.out.println(newdata);
+// find even number by list
+		//List<Integer>data=Arrays.asList(10,5,23,4,6,20,27);
+		//List<Integer>newdata=data.stream().filter(n->n%2==0).collect(Collectors.toList());
+		//System.out.println(newdata);
+	    //}
+		
+// find odd number by list
+		//List<Integer> data = Arrays.asList(10, 5, 23, 4, 6, 20, 27);
+		//List<Integer> newdata = data.stream().filter(n -> n % 2 != 0).collect(Collectors.toList());
+		//System.out.println(newdata);
+		//}
+		
+// How many times Suraj occurs(name doesn't matter in uper & lower case )
+		//List<String> data = Arrays.asList("milk","Suraj","welcome","Sohan","Suraj");
+		//List<String> newdata = data.stream().filter(n ->n.equalsIgnoreCase("Suraj")).collect(Collectors.toList());
+		//System.out.println(newdata);
+		//}
+		// o/p=[Suraj,Suraj]
+		
+// Give me name which is start from latter "s".[start name from capital here, so we write capital "S" in formula].		
+		//List<String> data = Arrays.asList("milk","Suraj","welcome","Sohan","Suraj");
+		//List<String> newdata = data.stream().filter(n ->n.startsWith("S")).collect(Collectors.toList());
+		//System.out.println(newdata);
+		//System.out.println(newdata.size());
+	    //}
+		// o/p= [Suraj, Sohan, Suraj],3
+		
+// Used Map Method
+		
+// All string convert to upper case		
+		//List<String> data = Arrays.asList("milk", "Suraj", "welcome", "sohan", "Suraj");
+		//List<String> newdata = data.stream().map(n ->n.toUpperCase()).collect(Collectors.toList());
+		//System.out.println(newdata);
+		//System.out.println(newdata.size());
+        // o/p= [MILK, SURAJ, WELCOME, SOHAN, SURAJ],5
+		
+//	Every data multiple by 2 from list.	
+		//List<Integer> data = Arrays.asList(10, 20, 30, 40);
+		//List<Integer> newData = data.stream().map(n -> n * 2).collect(Collectors.toList());
+		//System.out.println(newData);
+		
+//	other methods
+// sort some data by list		
+		
+		//List<Integer> data = Arrays.asList(100, 20,55, 200);
+		//List<Integer> newData = data.stream().sorted().collect(Collectors.toList());
+		//System.out.println(newData);
+		// o/p= [20, 55, 100, 200]
+		
+// Short some String by list
+		//List<String> data = Arrays.asList("Suraj","Deepa","Lipsa","Amrita");
+		//List<String> newData = data.stream().sorted().collect(Collectors.toList());
+		//System.out.println(newData);
+		
+// Give me unic record from the collection & value is duplicate.
+// remove duplicate elements by list
+		List<String> data = Arrays.asList("Suraj", "Deepa","Suraj","Lipsa","Deepa","Amrita");
+		List<String> newData = data.stream().distinct().collect(Collectors.toList());
+		System.out.println(newData);
 	}
-
 }
---
+++ src/com/java/practice/ReverseString.java
@@ -0,0 +1,71 @@
+package com.java.practice;
+
+public class ReverseString {
+	public static void main(String[] args) {
+// reverse the string
+		//String str = "I Am The Best";
+		//for (int i = str.length()-1; i >=0; i--) {
+			//System.out.print(str.charAt(i));
+	
+// find indux value		
+		//String str = "Priyanka";
+		//System.out.print(str.charAt(3));
+		
+//find length of string		
+		//String str ="Priyanka";
+		//System.out.print(str.length());
+		
+//reverse index number
+		//String Str ="Priyanka";
+		//for (int i = Str.length()-1; i>=0; i--) {
+		//System.out.print(i);
+		
+// count total word in a sentence [split method]
+		//String Str = "I Am Good Girl";
+	    //String[]s= Str.split(" ");
+	    //System.out.println(s.length);
+//remove white space in sentence or b/w word.	   
+		//String data = " I Am Good ";
+	    //String[]S= data.trim().split(" ");
+	    //System.out.println(S.length);	
+//		
+		//String str = "I Am Good Girl";
+		//String[]s= str.trim().split("  ");
+	    //System.out.println();
+	    //for(String x:s) {
+	     //System.out.println(x);
+// upper case convert into lower case		
+		//String str="I AM THE BEST";
+		//System.out.println(str.toLowerCase());
+
+//lower case convert into upper case		
+		//String str="how am i";
+		//System.out.println(str.toUpperCase());
+		
+		
+//		here white space
+		//String str="how  am  i";
+		//System.out.println(str);
+// remove white space		
+		//String str="  how am i";
+		//System.out.println(str);
+		//System.out.println(str.trim());// remove white space
+		
+//latter start from s ( s is prefix)
+		//String str="sssdddddhhhhcccyykk";
+		//System.out.println(str.startsWith("s"));
+		// o/p= true
+// latter end with k ( k is prefix)
+		//String Str="sssdddddhhhhcccyykk";
+		//System.out.println(Str.endsWith("k"));
+		
+// == & equals
+		
+		//String S1=new String ("kumar");
+		//String S2=new String ("kumar");
+		//System.out.println(S1==S2);
+		//System.out.println(S1.equals(S2));
+	
+		}
+		}
+	
